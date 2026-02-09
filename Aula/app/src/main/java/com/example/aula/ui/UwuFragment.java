cddcb6ea2a19d57ce6aed96502bb6ba64ac7dd89
Gabriela Vázquez <gvazquezi24@fpcoruna.afundacion.org>
2026-02-09 21:36:19 +0100

    uwu list

--- Aula/app/src/main/java/com/example/aula/ui/UwuFragment.java
+++ Aula/app/src/main/java/com/example/aula/ui/UwuFragment.java
@@ -2,12 +2,18 @@ package com.example.aula.ui;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.aula.R;
+import com.example.aula.data.InMemoryNoticeRepository;
+import com.example.aula.viewmodel.NoticeViewModel;
+import com.example.aula.viewmodel.NoticeViewModelFactory;
+import com.google.android.material.snackbar.Snackbar;
 
 public class UwuFragment extends Fragment {
 
@@ -18,5 +24,27 @@ public class UwuFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        TextView uwuText = view.findViewById(R.id.uwuText);
+
+
+        InMemoryNoticeRepository repo = InMemoryNoticeRepository.getInstance();
+
+        NoticeViewModelFactory noticeFactory =
+                new NoticeViewModelFactory(repo);
+
+        NoticeViewModel noticeVm =
+                new ViewModelProvider(this, noticeFactory)
+                        .get(NoticeViewModel.class);
+
+        noticeVm.getListado().observe(getViewLifecycleOwner(), uwuText::setText);
+
+        // Observa errores "persistentes" (no son eventos)
+        noticeVm.getError().observe(getViewLifecycleOwner(), err -> {
+            if (err != null) {
+                Snackbar.make(view, err, Snackbar.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
